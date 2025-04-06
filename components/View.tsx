d03e9628ec72eeb379862ac1722df373d0f7475e
Karan Panchal <karanhpanchal09@gmail.com>
2025-04-06 22:50:21 +0530

    Startup details Page completed

--- components/View.tsx
+++ components/View.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Ping from "./Ping";
+import { client } from "@/sanity/lib/client";
+import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 
-const View = ({ id }: { id: string }) => {
+const View = async({ id }: { id: string }) => {
+    const {views: totalViews} = await client.withConfig({useCdn:false}).fetch(STARTUP_VIEWS_QUERY, {id});
+
+    const viewLabel = totalViews === 1 ? "View" : "Views";
   return (
     <div className="view-container">
       <div className="absolute -top-2 -right-2">
         <Ping />
       </div>
       <p className="view-text">
-        <span className="font-black"></span>
+        <span className="font-black">{totalViews} {viewLabel}</span>
       </p>
     </div>
   );
