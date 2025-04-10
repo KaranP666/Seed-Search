e29344f6e20d2199bb3c1e817db0e1574e99b56b
Karan Panchal <karanhpanchal09@gmail.com>
2025-04-10 23:15:10 +0530

    fixed the toast

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
@@ -10,6 +10,7 @@ import React, { Suspense } from "react";
 import markdownit from 'markdown-it'
 import { Skeleton } from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 
 const md = markdownit()
 
@@ -17,7 +18,15 @@ export const experimental_ppr = true;
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  //implementing parallel req to fetch
+  const [post,{select : editorPosts}]  = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: 'startup-of-the-day'})
+  ]);
+
+  // const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  // const {select : editorPosts} = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {slug: 'startup-of-the-day'});
 
   if (!post) return notFound();
 
@@ -79,7 +88,24 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
       {/* Above is static next part is PPR */}
 
+      {
+        editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Startups of the day</p>
+            <ul className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard,index:number)=>(
+                <StartupCard key={index} post={post}/>
+              ))}
+            </ul>
+
+          </div>
+        )
+      }
+
       {/* Whenever we want to write anything dynamic wrap it in suspense */}
+
+
+
       
       <Suspense fallback={<Skeleton className="view_skeleton"/>}>
           <View id={id}/>
